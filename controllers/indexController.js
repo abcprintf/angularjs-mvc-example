81dee675d76d253165465ca6471b1b9db8c2bf80
Abcprintf <wh.cprintf@gmail.com>
2018-12-10 19:23:06 +0700

    update

---
+++ controllers/indexController.js
@@ -0,0 +1,8 @@
+var thisApp = angular.module('angularjs-mvc-example').controller('indexController', function ($scope, $http) {
+    
+    $scope.pagename = 'index page';
+
+    $(document).ready(function () {
+
+    });
+});
