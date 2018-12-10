3f94fa229742f3e702ebd2af37ac00bf65306045
Abcprintf <wh.cprintf@gmail.com>
2018-12-10 19:42:52 +0700

    add about

---
+++ controllers/aboutController.js
@@ -0,0 +1,8 @@
+var thisApp = angular.module('angularjs-mvc-example').controller('aboutController', function ($scope, $http) {
+    
+    $scope.pagename = 'about page';
+
+    $(document).ready(function () {
+
+    });
+});
