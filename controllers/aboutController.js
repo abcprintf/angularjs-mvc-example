a376bb6a2a9561ec1e3742703ef3511aeefdef45
Abcprintf <wh.cprintf@gmail.com>
2018-12-21 15:45:31 +0700

    delete

--- controllers/aboutController.js
+++
@@ -1,8 +0,0 @@
-var thisApp = angular.module('angularjs-mvc-example').controller('aboutController', function ($scope, $http) {
-    
-    $scope.pagename = 'about page';
-
-    $(document).ready(function () {
-
-    });
-});
